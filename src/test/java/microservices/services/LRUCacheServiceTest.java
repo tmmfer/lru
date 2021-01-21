6ea3898e2c3c3b434426e08b5ac189092454c904
Tiago Ferreira <tf1046980@bose.com>
2021-01-21 10:58:44 +0000

    updated tests data

--- src/test/java/microservices/services/LRUCacheServiceTest.java
+++ src/test/java/microservices/services/LRUCacheServiceTest.java
@@ -7,20 +7,39 @@ import org.junit.Test;
 
 public class LRUCacheServiceTest {
 
-    private NodeService service;
+    private LRUCacheService service;
 
     private static final String TEST_NODE_ID = "aoo1314zca-ad1-123131-achj";
 
     @Before
     public void initialize() {
-        service = new NodeService();
+        service = new LRUCacheService();
+    }
+
+    @Test
+    public void testNodeNotInCache() {
+
+        Node node = new Node();
+
+        boolean isInCache = service.isInCache(node.getId());
+
+        Assert.assertFalse(isInCache);
+    }
+
+    @Test
+    public void testNodeInCache() {
+        Node node = Node.NodeBuilder.aNode().withId(TEST_NODE_ID).build();
+        service.placeInCache(node);
+        Assert.assertTrue(service.isInCache(TEST_NODE_ID));
     }
 
     @Test
     public void testGetCachedNode() {
+        Node node = Node.NodeBuilder.aNode().withId(TEST_NODE_ID).build();
+        service.placeInCache(node);
 
-        Node node = service.getNodeById(TEST_NODE_ID);
+        Node nodeFromCache = service.getCachedNode(TEST_NODE_ID);
 
-        Assert.assertEquals(TEST_NODE_ID, node.getId());
+        Assert.assertEquals(TEST_NODE_ID, nodeFromCache.getId());
     }
 }
--- src/test/java/microservices/services/NodeServiceTest.java
+++ src/test/java/microservices/services/NodeServiceTest.java
@@ -7,39 +7,20 @@ import org.junit.Test;
 
 public class NodeServiceTest {
 
-    private LRUCacheService service;
+    private NodeService service;
 
     private static final String TEST_NODE_ID = "aoo1314zca-ad1-123131-achj";
 
     @Before
     public void initialize() {
-        service = new LRUCacheService();
-    }
-
-    @Test
-    public void testNodeNotInCache() {
-
-        Node node = new Node();
-
-        boolean isInCache = service.isInCache(node.getId());
-
-        Assert.assertFalse(isInCache);
-    }
-
-    @Test
-    public void testNodeInCache() {
-        Node node = Node.NodeBuilder.aNode().withId(TEST_NODE_ID).build();
-        service.placeInCache(node);
-        Assert.assertTrue(service.isInCache(TEST_NODE_ID));
+        service = new NodeService();
     }
 
     @Test
     public void testGetCachedNode() {
-        Node node = Node.NodeBuilder.aNode().withId(TEST_NODE_ID).build();
-        service.placeInCache(node);
 
-        Node nodeFromCache = service.getCachedNode(TEST_NODE_ID);
+        Node node = service.getNodeById(TEST_NODE_ID);
 
-        Assert.assertEquals(TEST_NODE_ID, nodeFromCache.getId());
+        Assert.assertEquals(TEST_NODE_ID, node.getId());
     }
 }
